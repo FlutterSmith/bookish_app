5a6703aa101a832a89d5788ceb5976a7e73618a3
Your Name <you@example.com>
2024-06-09 18:07:53 +0300

    change theme & add text to spash view body

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookish/core/utils/assets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -8,10 +9,20 @@ class SplashViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(
           AssetsData.logo,
-          height: 300,
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 18.0,
+          ),
         ),
       ],
     );
