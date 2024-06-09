5222c1b706bac816b0e9a1432b3ef44df4af2484
Your Name <you@example.com>
2024-06-10 00:45:02 +0300

    add sliding animation text to splash view

---
+++ lib/Features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'Read Free Books',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          );
+        });
+  }
+}
