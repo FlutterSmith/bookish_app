7b43872400afe45d625d14cb718332bd1fdb727f
Your Name <you@example.com>
2024-06-10 02:27:56 +0300

    add custom app bar to home

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'widgets/home_view_body.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
   }
 }
