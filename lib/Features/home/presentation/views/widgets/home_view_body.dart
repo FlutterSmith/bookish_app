3d35190341fa2e14486d8b07bed04c7c93a3a044
Your Name <you@example.com>
2024-06-10 17:58:57 +0300

    add featured books list view to home

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookish/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookish/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookish/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,11 +8,15 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        CustomListViewItem(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+        ],
+      ),
     );
   }
 }
+
