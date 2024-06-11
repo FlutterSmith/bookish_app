5916c155b175d210018f44c6f0946115c3f5546e
Your Name <you@example.com>
2024-06-11 20:19:43 +0300

    add best seller list view item part 1

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookish/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookish/Features/home/presentation/views/widgets/featured_books_list_view.dart';
+import 'package:bookish/core/utils/assets.dart';
 import 'package:bookish/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -23,6 +24,48 @@ class HomeViewBody extends StatelessWidget {
           FeaturedBooksListView(),
           SizedBox(height: 40),
           Text('Best Seller', style: Styles.titleMedium),
+          SizedBox(height: 10),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.4 / 3.9,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                color: Colors.red,
+                image: DecorationImage(
+                  image: AssetImage(
+                    AssetsData.testImage2,
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          const Column(
+            children: [
+              Text('Harry Potter ', style: Styles.titleMedium),
+              Text('J.KRowling', style: Styles.titleMedium),
+              Text('19.99', style: Styles.titleMedium),
+            ],
+          ),
         ],
       ),
     );
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const String logo = 'assets/images/Logo.png';
   static const String testImage = 'assets/images/test1.jpeg';
+  static const String testImage2 = 'assets/images/harrypotter.jpg';
 }
