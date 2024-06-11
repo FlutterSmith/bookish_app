b9240fcf86adb374e583dc206af33cc332d4920c
Your Name <you@example.com>
2024-06-11 17:32:41 +0300

    add font family to our app

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -50,11 +50,11 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 800),
+      duration: const Duration(milliseconds: 400),
     );
 
     slidingAnimation = Tween<Offset>(
-      begin: const Offset(0, 2),
+      begin: const Offset(0, 4),
       end: Offset.zero,
     ).animate(animationController);
 
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class Styles {
   static const titleMedium = TextStyle(
-    fontSize: 20,
+    fontSize: 20 ,
     fontWeight: FontWeight.w600,
     fontFamily: 'Montserrat',
   );
