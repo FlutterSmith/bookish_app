7e9ebccf468b5ed44a5c2b520d9f354ca2b10015
Your Name <you@example.com>
2024-06-13 17:02:31 +0300

    create details book view

---
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookish/Features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
