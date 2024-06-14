31b4dbf56e4016bef30144ccbee38b3030f8da4e
Your Name <you@example.com>
2024-06-14 21:58:36 +0300

    add title and subtitle

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,7 +1,10 @@
+import 'package:bookish/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookish/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookish/Features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookish/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -23,10 +26,37 @@ class BookDetailsViewBody extends StatelessWidget {
           Padding(
             padding: EdgeInsets.only(
               left: width * 0.22,
-              right: width * 0.22, 
+              right: width * 0.22,
               bottom: 20,
             ),
             child: const CustomBookImage(),
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle16.copyWith(
+              fontStyle: FontStyle.italic,
+              color: Colors.grey.shade300,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(FontAwesomeIcons.solidStar, color: Colors.yellow, size: 15),
+              SizedBox(width: 6.3),
+              Text('4.8', style: Styles.textStyle20),
+              SizedBox(width: 5),
+              Text('(2000)', style: Styles.textStyle14),
+            ],
           ),
         ],
       ),
--- lib/Features/home/presentation/views/widgets/book_rating.dart
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -11,6 +11,7 @@ class BookRating extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Row(
+      
       children: [
         Icon(FontAwesomeIcons.solidStar, color: Colors.yellow),
         SizedBox(width: 6.3),
