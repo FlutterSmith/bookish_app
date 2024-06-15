2f1181d045e79a998dc3c216ac3407ff04bd7b81
Your Name <you@example.com>
2024-06-16 00:28:41 +0300

    searcvh view ui done

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,10 @@
+import 'package:bookish/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookish/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookish/core/utils/app_router.dart';
+import 'package:bookish/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -14,9 +19,46 @@ class SearchViewBody extends StatelessWidget {
         bottom: 10,
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          SizedBox(height: 30),
+          Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Text(
+              'Search Result',
+              style: Styles.textStyle20,
+            ),
+          ),
+          Expanded(child: SearchListViewItem()),
         ],
+      ),
+    );
+  }
+}
+
+class SearchListViewItem extends StatelessWidget {
+  const SearchListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 10,
+          top: 10,
+        ),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return const BestSellerListViewItem();
+          },
+          itemCount: 10,
+          padding: EdgeInsets.zero,
+        ),
       ),
     );
   }
