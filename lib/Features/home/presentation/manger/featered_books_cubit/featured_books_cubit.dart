e2c2055d29d729ad98d2e43bcd967ab52d63595f
Your Name <you@example.com>
2024-06-19 02:49:22 +0300

    create featured get  method

--- lib/Features/home/presentation/manger/featered_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featered_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookish/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookish/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> getFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.getFeaturedBooks();
+
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksFailed(failure.errMessage));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      },
+    );
+  }
 }
