863a644c860223cdcb77bfd13f716685612307a2
Your Name <you@example.com>
2024-06-18 20:45:50 +0300

    implement get newest books

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+
+  final Dio _dio;
+
+  ApiService(this._dio);
+
+  Future<Map<String, dynamic>> getBooks({required String endPoint}) async {
+    Response response = await _dio.get('$_baseUrl$endPoint');
+    return response.data;
+  }
+}
