f945dc6c2d991970db7c0244033de96c91b0cec6
Your Name <you@example.com>
2024-06-20 23:46:21 +0300

    cashed network image

--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,24 +1,27 @@
-import 'package:bookish/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key});
+  const CustomBookImage({super.key, required this.imageUrl});
+
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(16)),
-          color: Colors.red,
-          image: DecorationImage(
-            image: AssetImage(
-              AssetsData.testImage,
+      aspectRatio: 2.5 / 4,
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        imageBuilder: (context, imageProvider) => Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.fill,
             ),
-            fit: BoxFit.fill,
           ),
         ),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
     );
   }
